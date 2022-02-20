96c48e585c3542f3405e009e4c5e2c62da8707f6
ale-ch <alessiochiodin@hotmail.com>
2022-02-20 10:29:23 +0100

    Update file_backup_utility.py
    Refactor code into multiple functions.

--- file_backup_utility.py
+++ file_backup_utility.py
@@ -10,6 +10,8 @@ print('''
 
 import shutil, os, time, copy
 
+print('Current working directory: ' + os.getcwd())
+
 path = input('Enter full path to working directory (wd): ').replace('\\', '//')
 print('File list\n', os.listdir(path))
 
@@ -24,59 +26,73 @@ sleep_time = int(input("Enter frequency of backup (seconds): "))
 # set working directory
 os.chdir(path)
 
-# if no backup path specified, create a 'backups' folder in working directory
-if len(backup_path_input) == 0:
-    backup_dir = 'backups'
-    backup_path_input = './/backups'
-# case full path is specified
-elif len(backup_path_input) > 1:
-    backup_dir = backup_path_input.split('//')
-    backup_dir = backup_dir[len(backup_dir)-1]
-    backup_path_input = backup_path_input.replace('\\', '//')
-# case input backup path consists of just backup folder name
-else:
-    backup_dir = copy.deepcopy(backup_path_input)
-    backup_path_input = './/' + backup_path_input
+def make_file_list(path, file_list_input, backup_dir_name):
+    #### MAKE A FILE LIST WITH THE INPUT FILE NAMES ####
 
-# if the path to backup directory does not exist create the directory tree
-if not os.path.exists(backup_path_input):
-    os.makedirs(backup_path_input)
-
-# if no file is specified select all files inside of wd
-# except for backup folder if it's inside wd
-if len(file_list_input) == 0:
-    if os.path.exists(os.path.join(path, backup_dir)) == True:
-        file_list = os.listdir(path)
-        file_list.remove(backup_dir)
+    # if no file is specified select all files inside of wd
+    # except for backup folder if it's inside wd
+    if len(file_list_input) == 0:
+        if os.path.exists(os.path.join(path, backup_dir_name)) == True:
+            file_list = os.listdir(path)
+            file_list.remove(backup_dir_name)
+        else:
+            file_list = os.listdir(path)
+    # otherwise make a list with input file names
     else:
-        file_list = os.listdir(path)
-else:
-    file_list = file_list_input.strip()
-    file_list = file_list.replace(' ', '')
-    file_list = file_list.split(',')
+        file_list = file_list_input.strip()
+        file_list = file_list.replace(' ', '')
+        file_list = file_list.split(',')
 
-print(file_list)
+    return file_list
 
-################# backup function #################
-def backup(path, file_list, file_list_input, backup_path_input, j):
+def get_backup_main_dir(backup_path_input):
+    #### RETURN MAIN BACKUP DIRECTORY AND BACKUP DIRECTORY NAME ####
+
+    # if no backup path specified, create a 'backups' folder in working directory
+    if len(backup_path_input) == 0:
+        backup_dir_name = 'backups'
+        backup_main_dir = './/backups'
+    # case full path is specified
+    elif len(backup_path_input) > 1:
+        backup_dir_name = backup_path_input.split('//')
+        backup_dir_name = backup_dir_name[len(backup_dir_name)-1]
+        backup_main_dir = backup_path_input.replace('\\', '//')
+    # case input backup path consists of just backup folder name
+    else:
+        backup_dir_name = copy.deepcopy(backup_path_input)
+        backup_main_dir = './/' + backup_path_input
+
+    return backup_main_dir, backup_dir_name
+
+def make_backup_path(path, file_list, backup_main_dir, backup_dir_name):
+    #### CREATE BACKUP DIRECTORY TREE ####
+
     # if file list consists of one element don't create a subfolder
     if len(file_list) == 1:
-        backup_path = copy.deepcopy(backup_path_input)
+        backup_path = copy.deepcopy(backup_main_dir)
     # if file list is all the content of wd (except for backup folder if it's inside wd)
     elif len(file_list) == len(os.listdir(path)) or len(file_list) == len(os.listdir(path))-1:
         path_list = path.split('//')
         # name backup subfolder as wd_backup_j
         backup_subdir = path_list[len(path_list)-1] + '_' + 'backup' + '_' + str(j)
         # full path to j-th backup subfolder
-        backup_path = os.path.join(backup_path_input, backup_subdir).replace('\\', '//')
-        os.makedirs(backup_path)
+        backup_path = os.path.join(backup_main_dir, backup_subdir).replace('\\', '//')
     # if file list is not one element and is not all content of wd
     else:
         # backup subfolder for j-th backup --- i.e. backups_1, backups_2, etc.
-        backup_subdir = backup_dir + "_" + str(j)
+        backup_subdir = backup_dir_name + "_" + str(j)
         # full path to j-th backup subfolder
-        backup_path = os.path.join(backup_path_input, backup_subdir).replace('\\', '//')
+        backup_path = os.path.join(backup_main_dir, backup_subdir).replace('\\', '//')
+
+    # if the path to backup directory does not exist create the directory tree
+    if not os.path.exists(backup_path):
         os.makedirs(backup_path)
+
+    return backup_path
+
+
+def backup(backup_path, file_list, j):
+    #### BACKUP FILES IN FILE LIST TO BACKUP PATH ####
 
     # create empty lists
     file_list_mod = [None] * len(file_list) # modified file names
@@ -90,6 +106,7 @@ def backup(path, file_list, file_list_input, backup_path_input, j):
                 file_list_mod[i] = file_list[i].split('.')[0] + "_" + str(j)
             else:
                 file_list_mod[i] = file_list[i]
+
             # destination path
             backup_file[i] = os.path.join(backup_path, file_list_mod[i]).replace('\\', '//')
             # copy file
@@ -102,6 +119,7 @@ def backup(path, file_list, file_list_input, backup_path_input, j):
                 + file_list[i].split('.')[1]
             else:
                 file_list_mod[i] = file_list[i]
+
             # destination path
             backup_file[i] = os.path.join(backup_path, file_list_mod[i]).replace('\\', '//')
             # copy file
@@ -110,13 +128,18 @@ def backup(path, file_list, file_list_input, backup_path_input, j):
         print(backup_file[i])
     print(backup_path)
 
-###################################################
+
+backup_main_dir = get_backup_main_dir(backup_path_input)[0]
+backup_dir_name = get_backup_main_dir(backup_path_input)[1]
+file_list = make_file_list(path, file_list_input, backup_dir_name)
 
 j = 1 # index for backup subfolder
-while(True): # run script until execution is aborted
-    backup(path, file_list, file_list_input, backup_path_input, j)
-    print(backup_path_input)
-    print(len(file_list))
+while True: # run script until execution is aborted
+    backup_path = make_backup_path(path, file_list, backup_main_dir, backup_dir_name)
+    backup(backup_path, file_list, j)
+
     j = j + 1 # increase index at each backup
+
+    print('# Files copied: ' + str(len(file_list)))
+
     time.sleep(sleep_time) # pause execution for sleep_time seconds
-    
